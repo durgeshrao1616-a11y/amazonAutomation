40f27563be6ab46720583499a10b8b7ee6c56121
durgeshrao1616-a11y <durgeshrao1616@gmail.com>
2026-01-22 18:46:51 +0530

    Updated for file location.

--- src/main/java/com/amazon/utils/TestDataProvider.java
+++ src/main/java/com/amazon/utils/TestDataProvider.java
@@ -5,8 +5,9 @@ import org.testng.annotations.DataProvider;
 public class TestDataProvider {
 	@DataProvider(name = "loginData")
 	public Object[][] loginData() throws Exception {
+		
 		ExcelUtils excel = new ExcelUtils(
-				"C:\\Users\\durge\\eclipse-workspace\\amazonAutomation\\src\\test\\resources\\testdata.xlsx");
+				"src/test/resources/testdata/testdata.xlsx");
 		return excel.getSheetData("Login");
 	}
 }
